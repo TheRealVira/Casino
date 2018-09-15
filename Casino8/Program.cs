18e4036c060dc436b429c6f65b26eaa6b33b4c3f
TheRealVira <therealvira@gmail.com>
2018-09-15 19:04:38 +0200

    rebasing project

---
+++ Casino8/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Casino8
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
